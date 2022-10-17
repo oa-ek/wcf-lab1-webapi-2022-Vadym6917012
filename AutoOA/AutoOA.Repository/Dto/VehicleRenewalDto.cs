2025771a7cbb31be324e4f174c5aa490dbbd2e8a
Alexander Puchko <oleksandr.puchko@oa.edu.ua>
2022-10-18 01:43:56 +0300

    VehicleRenewalDto

---
+++ AutoOA/AutoOA.Repository/Dto/VehicleRenewalDto.cs
@@ -0,0 +1,29 @@
+﻿using AutoOA.Core;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoOA.Repository.Dto
+{
+    public class VehicleRenewalDto
+    {
+        public int VehicleId { get; set; }
+        public int BodyTypeId { get; set; }
+        public virtual ICollection<BodyType> BodyTypes { get; set; }
+        public int VehicleModelId { get; set; }
+        public VehicleModel VehicleModel { get; set; }
+        public int GearBicId { get; set; }
+        public virtual ICollection<GearBox> GearBoxes { get; set; }
+        public decimal Price { get; set; }
+        public bool isNew { get; set; }
+        public int Mileage { get; set; }
+        public string? IconPatch { get; set; }
+        public int FuelTypeId { get; set; }
+        public virtual ICollection<FuelType> FuelTypes { get; set; }
+        public string Color { get; set; }
+
+    }
+}

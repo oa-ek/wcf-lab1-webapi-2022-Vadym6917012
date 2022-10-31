d5c38ec3d21a6327d21c2476cc7b5659384dbffa
Alexander Puchko <oleksandr.puchko@oa.edu.ua>
2022-10-31 13:15:25 +0200

    Create

---
+++ AutoOA/AutoOA.Repository/Dto/RegionDto/RegionCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoOA.Repository.Dto.RegionDto
+{
+    public class RegionCreateDto
+    {
+        [Required(ErrorMessage = "Введіть назву")]
+        [StringLength(32, ErrorMessage = "Must be between 1 and 32 characters", MinimumLength = 1)]
+        public string? RegionName { get; set; }
+    }
+}
---
+++ AutoOA/AutoOA.UI/Controllers/RegionController.cs
@@ -0,0 +1,38 @@
+﻿using AutoOA.Core;
+using AutoOA.Repository.Dto.RegionDto;
+using AutoOA.Repository.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoOA.UI.Controllers
+{
+    public class RegionController : Controller
+    {
+        private readonly ILogger<RegionController> _logger;
+        private readonly RegionRepository _RegionRepository;
+        public RegionController(ILogger<RegionController> logger, RegionRepository RegionRepository)
+        {
+            _logger = logger;
+            _RegionRepository = RegionRepository;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create(RegionCreateDto reregionDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var fuelType = await _RegionRepository.AddRegionAsync(new Region
+                {
+                    RegionName = reregionDto.RegionName
+                });
+                return RedirectToAction("Index", "Region", new { id = fuelType.RegionId });
+            }
+            return View(reregionDto);
+        }
+    }
+}
